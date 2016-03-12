46ece75d2a10e5b1d63d6bb351cefcb92da94b0c
Patrick Helm <me@patrick-helm.de>
2016-03-13 00:20:45 +0100

    Add rebase strategy
    == Strategy: Rebase
    
    ```ruby
    git.checkout(master)
    git.fetch(upstream)
    git.reset_hard("#{upstream}/#{master}")
    
    pull_requests.each do |pull_request|
      git.remote_add("#{pull_request.remote} #{pull_request.remote_url}")
      git.fetch(pull_request.remote)
      git.checkout(pull_request.branch)
      git.rebase(master)
      git.checkout(master)
      git.rebase("#{pull_request.branch}")
    end
    ```

---
+++ spec/octo_merge/rebase_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe OctoMerge::Rebase do
+  include SetupExampleRepos
+
+  let(:expected_history) do
+<<-'EXPECTED_HISTORY'.strip
+* Adds sunglasses
+* Adds cowboy_hat
+* Adds piercing
+* Adds tattoo
+* Adds earrrings
+EXPECTED_HISTORY
+  end
+
+  it { is_expected.to eq(expected_history) }
+end

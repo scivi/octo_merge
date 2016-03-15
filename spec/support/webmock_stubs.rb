cff71e9fc8313af802eff590a0a77e27feba3031
Patrick Helm <me@patrick-helm.de>
2016-03-15 19:51:46 +0100

    Fixes octokit version in webmock stubs

--- spec/support/webmock_stubs.rb
+++ spec/support/webmock_stubs.rb
@@ -9,7 +9,7 @@ module WebmockStubs
       'Accept' => 'application/vnd.github.v3+json',
       'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
       'Content-Type' => 'application/json',
-      'User-Agent' => 'Octokit Ruby Gem 4.2.0'
+      'User-Agent' => "Octokit Ruby Gem #{Octokit::VERSION}"
     }
   end
 
